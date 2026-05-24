f064eda2d2af2cfa7093e1b17418ad2f8e17d49f
ViitoJooj <joaovitor819oqueres@gmail.com>
2026-05-24 17:18:37 -0300

    better errors and len verify

---
+++ mail/validate.go
@@ -0,0 +1,27 @@
+package validate
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Validate(email string) error {
+
+	var errs []error
+
+	if len(email) > 255 {
+		message := fmt.Sprintf("Emails cannot exceed 255 characters. (current %d)", len(email))
+		errs = append(errs, errors.New(message))
+	}
+
+	if len(email) < 10 {
+		message := fmt.Sprintf("Email addresses cannot be shorter than 10 characters. (current %d)", len(email))
+		errs = append(errs, errors.New(message))
+	}
+
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+
+	return nil
+}
